809a89b743ab8386f8382169fd4c8d68e09e3b16
Abbah Divine <abbahattamah@gmail.com>
2023-03-24 11:52:22 -0700

    stack.c commit

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top of the stack
+ * @head: stack head
+ * @counter: line no.
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
